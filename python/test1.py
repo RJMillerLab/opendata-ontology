d670ae6797b79b9b06bcc0d4373359c344bc75ed
Fatemeh <fnargesian@cs.toronto.edu>
2018-07-06 16:48:02 -0400

    node prob calculation

--- python/test1.py
+++ python/test1.py
@@ -19,7 +19,7 @@ tags = orgl.reduce_tag_vectors(domains)
 
 print("Basic clustering")
 keys, vecs = orgc.mk_tag_table(tags)
-num_clusters = 10
+num_clusters = 20
 c = orgc.basic_clustering(vecs, num_clusters)
 
 print("Cluster to graph")
@@ -28,9 +28,11 @@ gp = orgg.add_node_vecs(g, vecs)
 
 print("Computing reachability probs")
 tag_ranks = dict()
+tag_dists = dict()
 for domain in domains[:10]:
     tag_dist = orgg.get_tag_probs(gp, domain)
+    tag_dists[domain['tag']] = tag_dist
     tag_ranks[domain['tag']] = [i for i in range(len(tag_dist)) if tag_dist[i][0]==domain['tag']][0]
-print(tag_ranks)
+print("tag ranks: {}".format(tag_ranks))
 
 print("Done")
